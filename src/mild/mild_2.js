8e4016549cc7862d6000c5f40a87bd28a92a971d
Jared Cohen <jaredecohen17@gmail.com>
2021-12-01 22:59:20 -0500

    rem non des

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -71,7 +71,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   let obj = object;
+   delete obj.password;
+   return obj;
 }
 
 /**
