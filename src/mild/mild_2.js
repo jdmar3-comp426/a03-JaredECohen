996c3385c906e7143bd4f8f166dcf8465d27d506
Jared Cohen <jaredecohen17@gmail.com>
2021-12-01 23:17:30 -0500

    mild2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -70,7 +70,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let copy = Object.assign({}, object);
+   let copy = object;
    removeKey(copy,key);
    //delete copy.password;
    return copy;
