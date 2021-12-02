d1398dba523276cf44d22a9fdda4abed092dc94c
Jared Cohen <jaredecohen17@gmail.com>
2021-12-01 20:57:54 -0500

    minmax

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -40,10 +40,10 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let min_max = new Map();
-    min_max.set(max, Math.max(...numbers));
-    min_max.set(min, Math.min(...numbers));
-    return min_max;
+    //let min_max = new Map();
+    let max = Math.max(...numbers);
+    let min = Math.min(...numbers);
+    return ('min: ${min), max: ${max}');
 }   
 
 /**
