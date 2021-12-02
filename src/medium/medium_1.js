610c2a10afdb45215f5c7a955388a71551d8198f
Jared Cohen <jaredecohen17@gmail.com>
2021-12-02 00:12:36 -0500

    variance

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -60,3 +60,13 @@ export function getStatistics(array) {
     return obj;
 }
 
+function variance(array, mean) {
+    return array.map(function (sample) {
+        return Math.pow(mean - sample, 2);
+    })
+        .reduce(function sum(m, v) {
+            m += v;
+            return m;
+        }, 0) / array.length;
+}
+
